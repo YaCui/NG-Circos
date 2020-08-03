665d39ea7e05c6947b11a551776b175f06d97fb7
cuiya <cui_ya@163.com>
2020-08-02 20:02:36 -0700

    Add files via upload

---
+++ input_file_of_CbioPortal_demo/BACKGROUND01.js
@@ -0,0 +1,11 @@
+var BACKGROUND01 = [ "BACKGROUND01" , {
+  BginnerRadius: 205,
+  BgouterRadius: 173,
+  BgFillColor: "#F2F2F2",
+  BgborderColor : "#000",
+  BgborderSize : 0.3,
+  axisShow: "true",
+  axisWidth: 0.1,
+  axisColor: "#000",
+  axisNum: 8
+}];
