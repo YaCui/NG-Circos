231930e79a9442a6449e4e1dcd987a0375d7d5ae
cuiya <cui_ya@163.com>
2020-08-02 20:04:26 -0700

    Add files via upload

---
+++ input_file_of_module_demos/ARC01.js
@@ -0,0 +1,10 @@
+var ARC01 = [ "ARC01" , {
+  innerRadius: 145,
+  outerRadius: 155,
+} , [
+  {chr: "EGFR", start: "57", end: "167", color: "#CD8500", des: "Recep_L domain"},
+  {chr: "EGFR", start: "185", end: "338", color: "blue", des: "Furin-like domain"},
+  {chr: "EGFR", start: "361", end: "480", color: "#CD8500", des: "Recep_L domain"},
+  {chr: "EGFR", start: "505", end: "636", color: "yellow", des: "GF_recep_IV domain"},
+  {chr: "EGFR", start: "713", end: "965", color: "red", des: "Pkinase Tyr domain"},
+]];
---
+++ input_file_of_module_demos/SCATTER01.js
@@ -0,0 +1,20 @@
+var SCATTER01 = [ "SCATTER01" , {
+  SCATTERRadius: 220,
+  innerCircleSize: 1,
+  outerCircleSize: 7,
+  innerCircleColor: "red",
+  outerCircleColor: "#CC3399",
+  innerPointType: "circle", //circle,rect
+  outerPointType: "circle", //circle,rect
+  innerrectWidth: 2,
+  innerrectHeight: 2,
+  outerrectWidth: 10,
+  outerrectHeight: 10,
+  outerCircleOpacity: 1,
+  random_data: 0
+} , [
+  {chr: "1", start: "1102484", end: "1102578", name: "hsa-mir-200b", des: "breast cancer"},
+  {chr: "11", start: "122017230", end: "122017301", name: "hsa-let-7a-2", des: "ovarian cancer"},
+  {chr: "22", start: "46508629", end: "46508702", name: "hsa-let-7a-3", des: "leukemia cancer"},
+  {chr: "14", start: "101349316", end: "101349412", name: "hsa-mir-127", des: "breast cancer"},
+]];
