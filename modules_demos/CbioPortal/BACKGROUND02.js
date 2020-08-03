af5da95d357338667b3e6655c024b3528ee2e28b
cuiya <cui_ya@163.com>
2020-08-02 20:06:36 -0700

    Add files via upload

---
+++ modules_demos/CbioPortal/BACKGROUND02.js
@@ -0,0 +1,11 @@
+var BACKGROUND02 = [ "BACKGROUND02" , {
+  BginnerRadius: 155,
+  BgouterRadius: 116,
+  BgFillColor: "#F2F2F2",
+  BgborderColor : "#000",
+  BgborderSize : 0.3,
+  axisShow: "true",
+  axisWidth: 0.1,
+  axisColor: "#000",
+  axisNum: 8
+}];
---
+++ modules_demos/CbioPortal/LINK01.js
@@ -0,0 +1,19 @@
+var LINK01 = [ "LINK01" , {
+  LinkRadius: 100,
+  LinkFillColor: "#F26223",
+  LinkWidth: 3,
+  LinkType:"Q",
+  displayLinkAxis: true,
+  LinkAxisColor: "#B8B8B8",
+  LinkAxisWidth: 0.5,
+  LinkAxisPad: 3,
+  displayLinkLabel: false,
+  LinkLabelColor: "red",
+  LinkLabelSize: 13,
+  LinkLabelPad: 8,
+} , [
+  {fusion: "CD74--NRG1", g1chr: "5", g1start: "149782758", g1end: "149782758", g1name: "CD74", g2chr: "8", g2start: "32513214", g2end: "32513214", g2name: "NRG1"},
+  {fusion: "CD74--ROS1", g1chr: "5", g1start: "149783763", g1end: "149783763", g1name: "CD74", g2chr: "6", g2start: "117647234", g2end: "117647234", g2name: "ROS1"},
+  {fusion: "CD74--NRG1", g1chr: "8", g1start: "32549502", g1end: "32549502", g1name: "CD74", g2chr: "5", g2start: "149782966", g2end: "149782966", g2name: "NRG1"},
+  {fusion: "ROS1--SLC34A2", g1chr: "4", g1start: "25667671", g1end: "25667671", g1name: "ROS1", g2chr: "6", g2start: "117662059", g2end: "117662059", g2name: "SLC34A2"},
+]];
