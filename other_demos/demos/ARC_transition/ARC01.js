4077eb59da2c36019e76c28d1a26c1d48e0f3f78
cuiya <cui_ya@163.com>
2020-08-02 20:18:04 -0700

    Add files via upload

---
+++ other_demos/demos/ARC_transition/ARC01.js
@@ -0,0 +1,14 @@
+ARC01 = [ "ARC01" , {
+  innerRadius: -25,
+  outerRadius: -15,
+  ARCAnimationDisplay: true,
+  ARCAnimationTime: 1000,
+  ARCAnimationDelay: 500,
+  ARCAnimationType: "linear",  //linear,circle,elastic,bounce
+} , [
+  {chr: "EGFR", start: "57" , end: "167", color: "#CD8500", des: "Recep_L domain"},
+  {chr: "EGFR", start: "185" , end: "338", color: "blue", des: "Furin-like domain"},
+  {chr: "EGFR", start: "361" , end: "480", color: "#CD8500", des: "Recep_L domain"},
+  {chr: "EGFR", start: "505" , end: "636", color: "yellow", des: "GF_recep_IV domain"},
+  {chr: "EGFR", start: "713" , end: "965", color: "red", des: "Pkinase Tyr domain"},
+]];
---
+++ other_demos/demos/LINK_supplementary/LINK02.js
@@ -0,0 +1,43 @@
+var LINK03 = [ "LINK03" , {
+  LinkRadius: 140,
+  LinkFillColor: "#b7ae5e",
+  LinkWidth: 2,
+  displayLinkAxis: true,
+  LinkAxisColor: "#B8B8B8",
+  LinkAxisWidth: 0.5,
+  LinkAxisPad: 0,
+  displayLinkLabel: true,
+  LinkLabelColor: "red",
+  LinkLabelSize: 13,
+  LinkLabelPad: 1,
+  LINKAnimationDisplay: true,
+  LINKAnimationDirection:"1to2", //1to2,2to1  [side1 to side2/side2 to side1]
+  LINKAnimationTime: 2000,
+  LINKAnimationDelay: 20,
+  LINKAnimationType: "linear", 
+} , [
+  // {fusion: "FGFR3--TACC3", g1chr: 3, g1start: 11795662, g1end: 11808986, g1name: "FGFR3", g2chr: 5, g2start: 11723217, g2end: 11746905, g2name: "TACC3"},
+  {fusion: "CLCN6-BRCF", g1chr: 1, g1start: 11806211, g1end: 11807190, g1name: "CLCN6", g2chr: 7, g2start: 140730665, g2end: 140781693, g2name: "BRCF"},
+  // {fusion: "CCDC6--RET", g1chr: 1, g1start: 43515894, g1end: 43623714, g1name: "RET", g2chr: "X", g2start: 63552678, g2end: 64666182, g2name: "TACC3"},
+]];
+
+ var LINK04 = [ "LINK04" , {
+  LinkRadius: 140,
+  LinkFillColor: "#ff7f0e",
+  LinkWidth: 2,
+  displayLinkAxis: true,
+  LinkAxisColor: "#B8B8B8",
+  LinkAxisWidth: 0.5,
+  LinkAxisPad: 0,
+  displayLinkLabel: true,
+  LinkLabelColor: "red",
+  LinkLabelSize: 13,
+  LinkLabelPad: 1,
+  LINKAnimationDisplay: true,
+  LINKAnimationDirection:"1to2", //1to2,2to1  [side1 to side2/side2 to side1]
+  LINKAnimationTime: 2000,
+  LINKAnimationDelay: 20,
+  LINKAnimationType: "linear", 
+} , [
+  {fusion: "EML4--ALK", g1chr: 2, g1start: 42169353, g1end: 42332548, g1name: "EML4", g2chr: 2, g2start: 29192774, g2end: 29921586, g2name: "ALK",html:"<table class=\"table\">\n<thead class=\"thead-light\">\n<tr>\n<th scope=\"col\">Gene</th>\n<th scope=\"col\">Chromosome</th>\n<th scope=\"col\">Start position</th>\n<th scope=\"col\">End position</th>\n</tr>\n</thead>\n<tbody>\n<tr>\n<th scope=\"row\">EML4</th>\n<td>2</td>\n<td>42169353</td>\n<td>42332548</td>\n</tr>\n<tr>\n<th scope=\"row\">ALK</th>\n<td>2</td>\n<td>29192774</td>\n<td>29921586</td>\n</tr>\n</tbody>\n</table>\n<figure class=\"figure\">\n<img src=\"./3.jpg\" class=\"figure-img img-fluid rounded\" alt=\"Three-dimensional structure of EML4--ALK.\">\n<figcaption class=\"figure-caption text-right\">Three-dimensional structure of proteins.</figcaption>\n</figure>",link:"http://www.rcsb.org/structure/2JIT"},
+]];
